e12410a5744494c42956301248cbd49e0021665a
Marcos <aurelio.marcos@ufv.br>
2020-09-24 09:47:43 -0300

    Acesso ao ID

--- src/main/java/br/com/livro/carros/service/dto/CarroDto.java
+++ src/main/java/br/com/livro/carros/service/dto/CarroDto.java
@@ -18,6 +18,10 @@ public class CarroDto {
         this.descricao = carro.getDescricao();
     }
 
+    public Long getId() {
+        return id;
+    }
+
     public String getNome() {
         return nome;
     }
